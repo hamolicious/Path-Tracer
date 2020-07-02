885cf327368d7e276c903b6e95b7ae4aec343d3a
Viktor Patmalnieks <56944714+hamolicious@users.noreply.github.com>
2020-07-02 05:29:47 +0100

    Add files via upload

--- settings.py
+++ settings.py
@@ -1,11 +1,12 @@
 
-# camera settings
+#region camera settings
 render_distance = 100 # maximum distance a ray is allowed to travel
 fov = 90 # cameras field of view
 rendering_canvas_size = (300, 300) # resolution of final render
 ray_reflections = 5 # amount of reflections rays are allowed to perform
+#endregion
 
-# environment settings
+#region environment settings
 sky_colour = [200, 200, 230] # colour of background
 
 """
@@ -25,14 +26,17 @@ shapes = [
 
 light = f'{rendering_canvas_size[0]/5} 0 {render_distance/4}'
 light_strength = 80
+#endregion
 
-# display screen settings
+#region display screen settings
 screen_size = (600, 600) # size to stretch the final render to and screen size to be used for displaying
 threads_to_open = 30 # amount of threads to open to render the image
                      # each thread will be responsible for ((canvas_width * canvas_height) / threads_to_open) amount of pixels
+#endregion
 
-# miscelenious settings
+#region miscelenious settings
 play_sound_when_done = True # will play a tone when rendering is finished
+#endregion
 
 
 
